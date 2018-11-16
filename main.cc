4d93606f3c70d7026d323ac9982b350b35639fc6
MOTTE Antoine <amotte@akuery.pedago.ipb.fr>
2018-11-16 14:46:39 +0100

    allègement du main

--- main.cc
+++ main.cc
@@ -5,8 +5,6 @@ int main(int argc, char * argv[])
 {
   MPI_Status status;
   MPI_Init(&argc,&argv);
-  int NbLignes, NbCol, rec;
-  double Lx, Ly, Dt, tmax;
 
   DataFile file(argv[1]);
   file.ReadDataFile();
