4da9f1f16617fe8350f43cf7fbd0c1b2f51534ec
KUHN Fanny <fkuhn@laudanum.pedago.ipb.fr>
2018-11-28 17:00:46 +0100

    a faire tourner

--- main.cc
+++ main.cc
@@ -3,10 +3,15 @@
 
 int main(int argc, char * argv[])
 {
+  int Me;
   MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD,&Me);
+  double tstart, tend, time, t_total;
 
   DataFile file(argv[1]);
   file.ReadDataFile();
+
+  tstart=MPI_Wtime();
 
   Probleme Pb1(file);
   Pb1.charge();
@@ -14,6 +19,13 @@ int main(int argc, char * argv[])
 
   Pb1.TimeIteration();
 
+  tend=MPI_Wtime();
+  t_total=tend-tstart;
+  MPI_Allreduce(&t_total,&time,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+  if(Me==0)
+  {
+    printf("Total Elapsed Time = %f \n", time);
+  }
   Pb1.PostProcessing();
 
   MPI_Finalize();
